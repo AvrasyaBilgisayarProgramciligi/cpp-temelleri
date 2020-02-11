8327178b2576d2a984a386e25e62d7b46903e752
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-02-11 15:37:39 +0300

    Operatörler bölümü tamamlandı

---
+++ bolum-01/02_operatorler.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /* Atama operatörü (=)
+     * Bir değişkene herhangi bir değer atanacağı 
+     * zaman kullanılan operatör.
+     */
+    int sayi = 2;
+    char harf = 'c';
+
+    /* Aritmetik operatörler
+     * Toplama  (+)
+     * Çıkarma  (-)
+     * Çarpma   (*)
+     * Bölme    (/)
+     * Mod Alma (%)
+     */
+
+    /* Örnek;
+     * Verilen iki değerin dört işlemde sonucunu hesaplayınız.
+     */
+
+    int sayi_1 = 10, sayi_2 = 5;
+
+    cout << "Toplamı: " << sayi_1 + sayi_2 << endl;
+    cout << "Farkı: " << sayi_1 - sayi_2 << endl;
+    cout << "Çarpımı: "<< sayi_1 * sayi_2 << endl;
+    cout << "Bölümü: " << sayi_1 / sayi_2 << endl;
+
+    /* Aritmetik atama operatörleri
+     * Tople ve ata  (+=)
+     * Çıkar ve ata  (-=)
+     * Çarp ve ata   (*=)
+     * Böl ve ata    (/=)
+     * Mod al ve ata (%=)
+     */
+
+    /* Karşılaştırma operatörleri
+     * Küçüktür operatörü      (<)
+     * Büyüktür operatörü      (>)
+     * Küçük eşittir operatörü (<=)
+     * Büyük eşittir operatörü (>=)
+     * Eşit mı operatörü       (==)
+     * Farklı mı operatörü     (!=) 
+     */
+
+    //Örnekler;
+    bool a = (6 < 3);
+    cout << "6 3' ten küçük mü?: " << a << endl;
+    a = (9 > 5);
+    cout << "9 5' ten büyük mü?: " << a << endl;
+
+    return 0;
+}
