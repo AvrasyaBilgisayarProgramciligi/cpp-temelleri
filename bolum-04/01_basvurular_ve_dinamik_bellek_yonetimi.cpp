85b8b0677470c6443a5759380bcb7bd05b89c4c6
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-02-21 22:20:08 +0300

    Basvurular ve dinamik bellek yönetimi fakat kısım bitirilmedi

---
+++ bolum-04/01_basvurular_ve_dinamik_bellek_yonetimi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* Aslında basit düşündüğümüzde başvurular göstericilerle aynı işi yapar,
+ * fakat göstericilere karşı üstünlükleri vardır ve anlaşılır programlar
+ * yazmak için daha uygundur.
+ */
+
+// Örnek:
+void gosterici(double *d);
+void basvuru(double &d);
+
+int main()
+{
+	
+}
