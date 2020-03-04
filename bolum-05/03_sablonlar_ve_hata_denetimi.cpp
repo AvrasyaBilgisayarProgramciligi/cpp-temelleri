796039b787bf16077a070079df4ae3def83899b6
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-03-04 22:23:41 +0300

    Şablonlar ve hata denetimi eklendi

--- bolum-05/03_sablonlar_ve_hata_denetimi.cpp
+++ bolum-05/03_sablonlar_ve_hata_denetimi.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+
+/* Sosyal fonksiyonlar
+ * Aynı fonksiyonu farklı veri yapıları ile işleyebilecek bir yapı olduğundan,
+ * tekrar tekrar kod yazmamızı engelleyip anlaşılır ve okunabilir kod yazmamızı sağlar.
+ */
+
+int main()
+{
+
+}
